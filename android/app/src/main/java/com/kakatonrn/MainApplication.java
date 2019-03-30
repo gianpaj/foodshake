ebe85e5112cc8b34e22fe7cca91af7d9a4639383
Gianfranco Palumbo <gianpa@gmail.com>
2019-03-30 18:03:26 +0200

    remove react-native-tensorflow

--- android/app/src/main/java/com/kakatonrn/MainApplication.java
+++ android/app/src/main/java/com/kakatonrn/MainApplication.java
@@ -3,7 +3,6 @@ package com.kakatonrn;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.rntensorflow.RNTensorFlowPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
@@ -26,7 +25,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RNTensorFlowPackage(),
             new VectorIconsPackage(),
             new RNGestureHandlerPackage()
       );
