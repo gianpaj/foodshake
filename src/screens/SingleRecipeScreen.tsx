251851466d2d92fe3d1060c722dd8f6b3ce88989
Gianfranco Palumbo <gianpa@gmail.com>
2019-03-30 14:24:05 +0200

    prettier

--- src/screens/SingleRecipeScreen.tsx
+++ src/screens/SingleRecipeScreen.tsx
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
-import { View, Text, Image, FlatList, StyleSheet } from 'react-native';
-import { content } from '../helpers/contentful';
-import { Button } from 'react-native-paper';
-import Icon from 'react-native-vector-icons/AntDesign';
-import { NavigationScreenProp, NavigationState } from 'react-navigation';
+import React, { Component } from "react";
+import { View, Text, Image, FlatList, StyleSheet } from "react-native";
+import { content } from "../helpers/contentful";
+import { Button } from "react-native-paper";
+import Icon from "react-native-vector-icons/AntDesign";
+import { NavigationScreenProp, NavigationState } from "react-navigation";
 
 interface Props {
   navigation: NavigationScreenProp<NavigationState>;
@@ -20,26 +20,30 @@ export default class SearchScreen extends Component<Props, IState> {
   constructor(props: any) {
     super(props);
     this.state = {
-      heading: 'Single Recipe',
-      fullText: [''],
-      ingredients: [''],
+      heading: "Single Recipe",
+      fullText: [""],
+      ingredients: [""],
       image: {
-        uri: 'https://facebook.github.io/react-native/docs/assets/favicon.png',
-      },
+        uri: "https://facebook.github.io/react-native/docs/assets/favicon.png"
+      }
     };
   }
-  text = 'Single Recipe';
+  text = "Single Recipe";
 
   componentDidMount() {
     content().then((entry: any) => {
       const uri = entry.fields.image.fields.file.url;
       this.setState({
         heading: entry.fields.name,
-        fullText: entry.fields.fullText.content.map((p: any) => p.content[0].value),
-        ingredients: entry.fields.ingredients.map((ingr: any) => ingr.fields.name),
+        fullText: entry.fields.fullText.content.map(
+          (p: any) => p.content[0].value
+        ),
+        ingredients: entry.fields.ingredients.map(
+          (ingr: any) => ingr.fields.name
+        ),
         image: {
-          uri: `https:${uri}`,
-        },
+          uri: `https:${uri}`
+        }
       });
     });
   }
@@ -66,8 +70,11 @@ export default class SearchScreen extends Component<Props, IState> {
   render() {
     return (
       <View>
-        <Button style={styles.button} onPress={() => this.props.navigation.goBack()}>
-          <Icon size={16} name={'arrowleft'} />
+        <Button
+          style={styles.button}
+          onPress={() => this.props.navigation.goBack()}
+        >
+          <Icon size={16} name={"arrowleft"} />
           Back to the list
         </Button>
         {this.ingredientsList()}
@@ -82,34 +89,34 @@ export default class SearchScreen extends Component<Props, IState> {
 const styles = StyleSheet.create({
   heading: {
     fontSize: 24,
-    color: '#3f2d20',
+    color: "#3f2d20",
     paddingLeft: 16,
     paddingTop: 16,
-    fontWeight: '500',
+    fontWeight: "500"
   },
   fullText: {
-    padding: 16,
+    padding: 16
   },
-  ingredientList: {paddingLeft: 8},
+  ingredientList: { paddingLeft: 8 },
   ingredient: {
-    backgroundColor: '#f5fae5',
+    backgroundColor: "#f5fae5",
     padding: 8,
     marginTop: 8,
     marginLeft: 8,
-    borderColor: '#9cd400',
+    borderColor: "#9cd400",
     borderWidth: 1,
     borderRadius: 4,
-    color: '#9cd400',
+    color: "#9cd400"
   },
   image: {
-    width: '100%',
+    width: "100%",
     height: 250,
-    marginTop: 16,
+    marginTop: 16
   },
   button: {
     marginTop: 8,
-    display: 'flex',
-    alignItems: 'flex-start',
-    justifyContent: 'flex-start',
-  },
+    display: "flex",
+    alignItems: "flex-start",
+    justifyContent: "flex-start"
+  }
 });
